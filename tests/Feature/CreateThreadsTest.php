4a9c69119f48d256c947780eb12110c1f9505638
Sale <aleksandargubecka@gmail.com>
2017-09-06 10:37:22 +0200

    User can publish treads.

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,14 +2,35 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guest_may_not_crrate_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+    }
+
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
+        $this->actingAs(factory('App\User')->create());
 
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
     }
 }
