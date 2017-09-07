177372648017e1282cbe94734b844173fcb893d3
Sale <aleksandargubecka@gmail.com>
2017-09-07 21:13:56 +0200

    Filter Threads by Channel.

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{channel}', 'ThreadsController@index');
 
 Route::post('/threads', 'ThreadsController@store');
 
