92f72da4cf1d2fd86f63c3630253ce32a0414255
Sale <aleksandargubecka@gmail.com>
2017-09-10 01:30:34 +0200

    Adding Validation errors and old data.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,17 +10,41 @@
                     <div class="panel-body">
                         <form method="post" action="/threads">
                             {{ csrf_field() }}
+
                             <div class="form-group">
                                 <label for="title">Title: </label>
-                                <input type="text" id="title" name="title" class="form-control" placeholder="title">
+                                <input type="text" id="title" name="title" class="form-control" placeholder="title"
+                                       value="{{ old('title') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="channel_id">Channel id: </label>
+                                <select name="channel_id" id="channel_id" class="form-control" required>
+                                    <option>Choose one...</option>
+                                    @foreach(\App\Channel::all() as $channel)
+                                        <option value="{{$channel->id}}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                            {{ $channel->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="form-group">
                                 <label for="body">Body: </label>
-                                <textarea name="body" id="body" class="form-control" rows="8"></textarea>
+                                <textarea name="body" id="body" class="form-control"
+                                          rows="8" required>{{ old('body') }}</textarea>
                             </div>
 
-                            <button class="btn btn-primary">Publish</button>
+                            <div class="form-group">
+                                <button class="btn btn-primary">Publish</button>
+                            </div>
                         </form>
+
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </div>
                 </div>
             </div>
